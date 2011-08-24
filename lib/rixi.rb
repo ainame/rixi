56cee2e166936506937fc5380f42f2e84149bd6a
ainame <ainame954@facebook.com>
2011-08-24 16:52:01 +0900

    t

--- lib/rixi.rb
+++ lib/rixi.rb
@@ -87,7 +87,7 @@ class Rixi
   #自分自身を返す
   def set_token(access_token, refresh_token)
     @token = OAuth2::AccessToken.new(@client,access_token,
-                                    :refreash_token => refresh_token,
+                                    :refresh_token => refresh_token,
                                     :expires_in => Time.now,
                                     :expires_at => 900,
                                     :mode => :header,
